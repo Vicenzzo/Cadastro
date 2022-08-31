d9f8329952cada1eeabfb7737a2c04cf12441e5f
Vicenzzo <vicenzzo.toth@gmail.com>
2022-08-31 11:26:41 -0300

    Primeiro Aquivo de teste

---
+++ teste.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+
+const app = express();
+
+
+app.listen(8081, () => {
+    console.log("O servidor está funcionando com prazer http://localhost:8081");
+});
+
+
+
